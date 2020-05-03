36f83df7bc2cab6d77b5aa2c323214558a0d2e20
zhen <myz6294369664myz@163.com>
2020-05-03 21:01:09 +0800

    使用shardingSphere集成读写分离，分库分表

--- admin/src/main/java/com/keeper/national/admin/web/testController.java
+++ admin/src/main/java/com/keeper/national/admin/web/testController.java
@@ -2,7 +2,9 @@ package com.keeper.national.admin.web;
 
 import cn.hutool.core.lang.Dict;
 import cn.hutool.core.util.StrUtil;
+import com.keeper.national.admin.domain.User;
 import com.keeper.national.admin.service.UserMapperService;
+import com.keeper.national.admin.util.RedisUtil;
 import com.keeper.national.common.constant.Status;
 import com.keeper.national.common.exception.JsonException;
 import com.keeper.national.common.util.ResponseResult;
@@ -10,6 +12,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -26,11 +30,20 @@ public class testController {
 
     @Autowired
     private UserMapperService userMapperService;
+    @Autowired
+    private RedisUtil redisUtil;
 
     @GetMapping("/user")
     @ApiOperation(value = "用户参数")
     public ResponseResult getUser(){
+        redisUtil.set("用户",userMapperService.list().toString());
         return ResponseResult.success(userMapperService.list());
+    }
+    @PostMapping("/insert")
+    @ApiOperation(value = "用户参数")
+    public ResponseResult saveUser(@RequestBody User user){
+        boolean save = userMapperService.save(user);
+        return ResponseResult.success(save);
     }
     /**
      * 测试方法
